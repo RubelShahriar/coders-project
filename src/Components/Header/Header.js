6aad00f2c8357f30a3c1f2f88cdb543cc6c02d42
RubelShahriar <mr.rubelshahriar@gmail.com>
2021-09-26 19:23:15 +0600

    cart change

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,9 +4,11 @@ import './Header.css';
 const Header = () => {
     return (
         <div className= 'heading'>
+            <div className= 'width'>
             <h2>Apollo It and Developers Farm</h2>
             <p>“We are a managed service provider with a passion for technology and developing elegant solutions to help support your growing IT needs. We focus on supporting non-profit organizations and SMBs. We’ve had the privilege to work with some excellent agencies.”</p>
             <h3>Total Budget: 10 Million</h3>
+            </div>
         </div>
     );
 };
